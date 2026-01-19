f2fda787ec5b2dfd775c35a50b580faa4a2cf96e
Alex <makargravanov@gmail.com>
2026-01-19 17:47:12 +0300

    update project structure

--- windows.bat
+++ windows.bat
@@ -1,1 +1,6 @@
-scons platform=windows use_mingw=true
+cd godot-cpp
+scons platform=windows use_mingw=yes target=template_release
+cd ..
+scons platform=windows use_mingw=yes target=template_release
+scons compiledb=yes platform=windows use_mingw=yes
+echo "ready"
