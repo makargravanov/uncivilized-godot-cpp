1434acef6a873ca26394f35e5d108eddaff55abe
Alex <makargravanov@gmail.com>
2026-01-23 20:16:23 +0300

    add GodotPtr

---
+++ src/util/GodotPtr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex on 23.01.2026.
+//
+
+#include "GodotPtr.h"
---
+++ src/util/GodotPtr.h
@@ -0,0 +1,70 @@
+//
+// Created by Alex on 23.01.2026.
+//
+
+#ifndef GODOTPTR_H
+#define GODOTPTR_H
+
+#include <utility>
+
+#include <godot_cpp/core/memory.hpp>
+
+template<typename T>
+class GodotPtr {
+public:
+    GodotPtr() : ptr(nullptr) {}
+
+    explicit GodotPtr(T* p) : ptr(p) {}
+
+    ~GodotPtr() {
+        reset();
+    }
+
+    GodotPtr(const GodotPtr&) = delete;
+    GodotPtr& operator=(const GodotPtr&) = delete;
+
+    GodotPtr(GodotPtr&& other) noexcept : ptr(other.ptr) {
+        other.ptr = nullptr;
+    }
+
+    GodotPtr& operator=(GodotPtr&& other) noexcept {
+        if (this != &other) {
+            reset();
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T* get() const { return ptr; }
+    T* operator->() const { return ptr; }
+    T& operator*() const { return *ptr; }
+
+    explicit operator bool() const { return ptr != nullptr; }
+
+    T* release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    void reset(T* p = nullptr) {
+        if (ptr) {
+            godot::memdelete(ptr);
+        }
+        ptr = p;
+    }
+
+    template<typename... Args>
+    static GodotPtr make(Args&&... args) {
+        return GodotPtr(memnew(T(std::forward<Args>(args)...)));
+    }
+
+private:
+    T* ptr;
+};
+
+
+
+
+#endif //GODOTPTR_H
