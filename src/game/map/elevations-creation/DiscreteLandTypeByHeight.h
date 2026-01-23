25cdb3bfb69895625aa97f5dff2aee0e4857cf64
Alex <makargravanov@gmail.com>
2026-01-23 19:45:40 +0300

    add new discrete land types to enumeration

--- src/game/map/elevations-creation/DiscreteLandTypeByHeight.h
+++ src/game/map/elevations-creation/DiscreteLandTypeByHeight.h
@@ -7,9 +7,12 @@
 #include "util/declarations.h"
 enum DiscreteLandTypeByHeight : u8 {
     OCEAN = 0,
-    VALLEY = 1,
-    PLAIN = 2,
-    HILL = 3,
-    MOUNTAIN = 4
+    SHALLOW_OCEAN = 1,
+    VALLEY = 2,
+    SHALLOW_VALLEY = 3,
+    PLAIN = 4,
+    HILL = 5,
+    MOUNTAIN = 6,
+    HIGH_MOUNTAIN = 7,
 };
 #endif //DISCRETEHEIGHTLANDTYPE_H
